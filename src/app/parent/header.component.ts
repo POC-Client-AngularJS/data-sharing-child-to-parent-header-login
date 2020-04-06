dbeba5f05b3e823a3aaef4e59ae3be8460a1c292
POC-Client-AngularJS <poc.client.angularjs@gmail.com>
2020-04-06 21:13:15 +0530

    Initial commit.

---
+++ src/app/parent/header.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-header",
+  templateUrl: "./header.component"
+})
+export class HeaderComponent implements OnInit {
+  ngOnInit {}
+}
