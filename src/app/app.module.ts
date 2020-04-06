f4ba24861b4f90fcb5b4c42ac2707a6155794a72
POC-Client-AngularJS <poc.client.angularjs@gmail.com>
2020-04-06 21:15:58 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { LoginComponent } from "./child/login.component";
+import { HeaderComponent } from "./parent/header.component";
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, LoginComponent],
+  declarations: [AppComponent, HelloComponent, LoginComponent, HeaderComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
